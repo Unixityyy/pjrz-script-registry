3a051f9d2b67977deb86c10823f3f790f762f3df
Unixity <103908889+Unixityyy@users.noreply.github.com>
2026-01-19 18:36:33 -0600

    Implement grip-based jump settings adjustment
    Corrected the jump multiplier and max jump speed based on grip value.

--- scripts/grip-speed.js
+++ scripts/grip-speed.js
@@ -1,1 +1,10 @@
-
+function tick(dt) {
+    if (RightController.GripValue >= 0.5) {
+        PlayerSettings.JumpMultiplier = 2:
+        PlayerSettings.MaxJumpSpeed = 7.5;
+    }
+    else {
+        PlayerSettings.JumpMultiplier = 1.5;
+        PlayerSettings.MaxJumpSpeed = 6.5;
+    }
+}
